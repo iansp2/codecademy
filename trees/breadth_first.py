6a67e38499a25e2edcd2d500c1240051b34a7238
iansp2 <ian.pisano@gmail.com>
2024-09-02 13:18:24 +0800

    finish breadth first search file

--- trees/breadth_first.py
+++ trees/breadth_first.py
@@ -1,2 +1,33 @@
 from tree_module import TreeNode
+from collections import deque
+
+def bfs(root_node, target_value):
+    frontier_path = deque()
+    initial_path = [root_node]    
+    frontier_path.appendleft(initial_path)
+
+    while frontier_path:
+        current_path = frontier_path.pop()
+        current_node = current_path[-1]
+
+        if current_node.value == target_value:
+            return current_path
+        
+        for child in current_node.children:
+            new_path = current_path.copy()
+            new_path.append(child)
+            frontier_path.appendleft(new_path)
+
+    return None
+
+sample_root_node = TreeNode("Home")
+docs = TreeNode("Documents")
+photos = TreeNode("Photos")
+sample_root_node.children = [docs, photos]
+my_wish = TreeNode("WishList.txt")
+my_todo = TreeNode("TodoList.txt")
+my_cat = TreeNode("Fluffy.jpg")
+my_dog = TreeNode("Spot.jpg")
+docs.children = [my_wish, my_todo]
+photos.children = [my_cat, my_dog]
 
--- trees/tree_module.py
+++ trees/tree_module.py
@@ -6,7 +6,7 @@ class TreeNode:
 		self.children = []
 
 	def add_child(self, child_node):
-		print("Adding " + str(child_node.value))
+		print("Adding " + child_node.value)
 		self.children.append(child_node)
 		
 	def remove_child(self, child_node):
@@ -20,24 +20,3 @@ class TreeNode:
 			current_node = nodes_to_visit.pop(0)
 			nodes_to_visit.extend(current_node.children)
 			print(current_node.value)
-
-def create_binary_tree(n):
-	if n <= 0:
-		return None
-	
-	root = TreeNode(random.randint(1, 100))
-	nodes = [root]
-
-	for _ in range(1, n):
-		new_node = TreeNode(random.randint(1, 100))
-		while True:
-			parent_node = random.choice(nodes)
-			if len(parent_node.children) < 2:
-				parent_node.add_child(new_node)
-				break
-		nodes.append(new_node)
-	
-	return root
-
-test_tree = create_binary_tree(8)
-test_tree.traverse()
